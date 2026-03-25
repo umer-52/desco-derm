2f3b7fb2f11753a31cc7074e6721fac49f9ddf6c
User <github@example.com>
2026-03-25 05:01:30 +0500

    Update product images

--- desco-web/next.config.ts
+++ desco-web/next.config.ts
@@ -1,6 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
+  basePath: "/desco-derm",
+  images: {
+    unoptimized: true,
+  },
   /* config options here */
 };
 
